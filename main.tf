74766027982de12709381923011d95cc75be5d4d
Aaron N. Brock <Contact@AaronNBrock.com>
2020-04-13 15:35:57 -0400

    fix tf 0.11 formatting

--- main.tf
+++ main.tf
@@ -47,8 +47,8 @@ resource "aws_instance" "jenkins" {
 # Elastic IP
 #
 resource "aws_eip_association" "eip_assoc" {
-  instance_id   = "${aws_instance.jenkins.id}"
-  allocation_id = "${aws_eip.jenkins.id}"
+  instance_id   = aws_instance.jenkins.id
+  allocation_id = aws_eip.jenkins.id
 }
 
 resource "aws_eip" "jenkins" {
