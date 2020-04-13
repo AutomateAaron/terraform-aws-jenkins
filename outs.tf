602ae319f6c8d1d6e07c9b39f3f47e142ffcc2b6
Aaron N. Brock <Contact@AaronNBrock.com>
2020-04-13 15:59:22 -0400

    update next_steps message

--- outs.tf
+++ outs.tf
@@ -16,8 +16,9 @@ output "url" {
 output "next_steps" {
   description = "The next steps to configure your jenkins instance."
   value       = <<EOL
+To complete jenkins setup:
 
-Step 1, get the initialAdminPassword:
+Step 1, get the initialAdminPassword (Note: You may need to wait few minutes before initializion is comleted):
 
   ssh -i ${var.ssh_key_name}.pem ubuntu@${aws_eip.jenkins.public_ip} "sudo cat \$(sudo docker volume inspect jenkins-data --format '{{ .Mountpoint }}')/secrets/initialAdminPassword"
 
